1c48c8efd9a1389eb0e5e2cbbb663339f54c9a81
HannaneHabibi <hananhabibi1999@gmail.com>
2024-10-17 12:07:21 +0200

    speech recognition

---
+++ Speech Recognition/api_secrets.py
@@ -0,0 +1,7 @@
+# We need 2 things. first Assembly AI API to fo the speech recognition and the request library from Pythin to talk to assembly AI's API.
+# https://www.assemblyai.com/
+# Open an account in AssembleyAi and get the API key and paste it here.
+API_KEY_ASSEMBLYAI = "17cf906b75d745a9924c54f18ddbe6c9"
+
+# we can start setting up how to upload transcribed and get the transcription from AssemblyAi as API.
+# Next, we are going to have a main file that is going to have all our code.
